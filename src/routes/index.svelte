e3479a193569b508e55fc467be5176b7e0e736e0
raz0229 <raja.zafar229@gmail.com>
2022-03-10 21:06:05 +0500

    notification bell

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,11 @@
 <script>
+  //import Cart from "$lib/cart.svelte"
   import Promo from "$lib/promo.svelte"
   import Deals from '$lib/deals.svelte'
   import Location from '$lib/location.svelte'
   import Testimonials from '$lib/testimonials.svelte'
 </script>
+<!-- <Cart/> -->
 
 <Promo/>
 
