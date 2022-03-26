67e6acdd4356716b2b155784d66562365bf749b0
raz0229 <raja.zafar229@gmail.com>
2022-03-26 22:15:23 +0500

    remote prices

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -38,148 +38,159 @@ export async function get() {
 
 const prices = await getDocuments()
 
-console.log(prices)
-    const products = [
+// template array
+let products = [
       {
         id: "00-00",
         title: "Chicken Tikka",
-        priceS: prices.get('00-00').priceS, priceM: prices.get('00-00').priceS, priceL: prices.get('00-00').priceS,
+        priceS: 0, priceM: 0, priceL: 0,
         img: "/menu/chicken-tikka.jpg"
       },
       {
         id: "00-01",
         title: "Chicken Fajita",
-        priceS: 400, priceM: 650, priceL: 950,
+        priceS: 0, priceM: 0, priceL: 0,
         img: "/menu/chicken-fajita.jpg"
       },
       {
         id: "00-02",
         title: "Fajita Sicilian",
-        priceS: 400, priceM: 650, priceL: 950,
+        priceS: 0, priceM: 0, priceL: 0,
         img: "/menu/fajita-sicilian.jpg"
       },
       {
         id: "00-03",
         title: "Hot n Spicy",
-        priceS: 400, priceM: 650, priceL: 950,
+        priceS: 0, priceM: 0, priceL: 0,
         img: "/menu/hot-n-spicy.jpg"
       },
       {
         id: "00-04",
         title: "Chicken Achari",
-        priceS: 400, priceM: 650, priceL: 950,
+        priceS: 0, priceM: 0, priceL: 0,
         img: "/menu/chicken-achari.jpg"
       },
       {
         id: "00-05",
         title: "Chicken Tandoori",
-        priceS: 400, priceM: 650, priceL: 950,
+        priceS: 0, priceM: 0, priceL: 0,
         img: "/menu/chicken-tandoori.jpg"
       },
       {
         id: "00-06",
         title: "Veggie Lover",
-        priceS: 400, priceM: 650, priceL: 950,
+        priceS: 0, priceM: 0, priceL: 0,
         img: "/menu/veggie-lover.jpg"
       },
       {
         id: "00-07",
         title: "Cheese Lover",
-        priceS: 450, priceM: 650, priceL: 1050,
+        priceS: 0, priceM: 0, priceL: 0,
         img: "/menu/cheese-lover.jpg"
       },
       {
         id: "01-00",
         title: "Bonfire Pizza",
-        priceS: 400, priceM: 650, priceL: 950,
+        priceS: 0, priceM: 0, priceL: 0,
         img: "/menu/bonfire.jpg"
       },
       {
         id: "01-01",
         title: "Super Supreme",
-        priceS: 450, priceM: 750, priceL: 1050,
+        priceS: 0, priceM: 0, priceL: 0,
         img: "/menu/super-supreme.jpg"
       },
       {
         id: "01-02",
         title: "Chicken Kebab",
-        priceS: 450, priceM: 750, priceL: 1050,
+        priceS: 0, priceM: 0, priceL: 0,
         img: "/menu/chicken-kebab.jpg"
       },
       {
         id: "01-03",
         title: "Creamy Pizza",
-        priceS: 450, priceM: 750, priceL: 1050,
+        priceS: 0, priceM: 0, priceL: 0,
         img: "/menu/creamy.jpg"
       },
       {
         id: "01-04",
         title: "Pepperoni Pizza",
-        priceS: 450, priceM: 750, priceL: 1050,
+        priceS: 0, priceM: 0, priceL: 0,
         img: "/menu/pepperoni.jpg"
       },
       {
         id: "01-05",
         title: "Extreme Pizza",
-        priceS: 450, priceM: 750, priceL: 1050,
+        priceS: 0, priceM: 0, priceL: 0,
         img: "/menu/extreme.jpg"
       },
       {
         id: "01-06",
         title: "Garlic Pizza",
-        priceS: 450, priceM: 750, priceL: 1050,
+        priceS: 0, priceM: 0, priceL: 0,
         img: "/menu/garlic.jpg"
       },
       {
         id: "01-07",
         title: "Chapli Pizza",
-        priceS: 450, priceM: 750, priceL: 1050,
+        priceS: 0, priceM: 0, priceL: 0,
         img: "/menu/chapli.jpg"
       },
       {
         id: "02-00",
         title: "Chicken Hotwings",
-        priceS: 250, priceM: 550, priceL: 850,
+        priceS: 0, priceM: 0, priceL: 0,
         img: "/menu/chicken-hotwings.jpg"
       },
       {
         id: "02-01",
         title: "Chicken Nuggets",
-        priceS: 210, priceM: 510, priceL: 810,
+        priceS: 0, priceM: 0, priceL: 0,
         img: "/menu/chicken-nuggets.jpg"
       },
       {
         id: "03-00",
         title: "Zinger Burger",
-        price: 450,
+        price: 0,
         img: "/menu/zinger-burger.jpg"
       },
       {
         id: "03-01",
         title: "500 ml drink",
-        price: 50,
+        price: 0,
         img: "/menu/500ml-drink.jpg"
       },
       {
         id: "03-02",
         title: "1 litre drink",
-        price: 70,
+        price: 0,
         img: "/menu/1l-drink.jpg"
       },
       {
         id: "03-03",
         title: "1.5 litre drink",
-        price: 100,
+        price: 0,
         img: "/menu/1.5l-drink.jpg"
       },
       {
         id: "03-04",
         title: "French Fries",
-        price: 150,
+        price: 0,
         img: "/menu/french-fries.jpg"
       }
     ];
   
+
+    // populate array with fetched prices
+    products.filter(obj => {
+      if (obj.price) obj.price = prices.get(obj.id).price;
+      else {
+        obj.priceS = prices.get(obj.id).priceS;
+        obj.priceM = prices.get(obj.id).priceM;
+        obj.priceL = prices.get(obj.id).priceL;
+      }
+    })
+
     return {
         status: 200,
         body: { 
