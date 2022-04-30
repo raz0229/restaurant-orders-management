c8e7ef488c9b8afa0d19e1a78eb51cfcf580cb1f
raz0229 <raja.zafar229@gmail.com>
2022-04-30 12:23:08 +0500

    scroll bottom floater

--- src/routes/deals.svelte
+++ src/routes/deals.svelte
@@ -52,6 +52,10 @@ const scrollHandler = () => {
   if (isInViewport()) loadMore = true;
 }
 
+const scrollToBottom = () => {
+  window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
+}
+
 </script>
 
 <svelte:window on:scroll="{ scrollHandler }"/>
@@ -67,4 +71,28 @@ const scrollHandler = () => {
 
 {#if loadMore}
 <Products bind:groups/>
+{:else}
+<div class="floater fixed bottom-4 left-4" on:click="{scrollToBottom}" >
+  <button
+          class="p-0 w-12 h-12 bg-indigo-500 dark:bg-dark-indigo hover:bg-indigo-700 rounded-full active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none">
+  <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 inline-block" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M20 12l-1.41-1.41L13 16.17V4h-2v12.17l-5.58-5.59L4 12l8 8 8-8z"/></svg>
+  </button>
+</div>
 {/if}
+
+<style>
+  .floater {
+    animation: bounce 1s infinite;
+  }
+ 
+@keyframes bounce {
+  0%, 100% {
+    transform: translateY(-25%);
+    animation-timing-function: cubic-bezier(0.8, 0, 1, 1);
+  }
+  50% {
+    transform: translateY(0);
+    animation-timing-function: cubic-bezier(0, 0, 0.2, 1);
+  }
+}
+</style>
