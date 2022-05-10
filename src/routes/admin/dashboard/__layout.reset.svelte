a1dd1444b67bc720a26d001b4a59694e07068c2c
raz0229 <raja.zafar229@gmail.com>
2022-05-10 17:37:54 +0500

    admin: settings

--- src/routes/admin/dashboard/__layout.reset.svelte
+++ src/routes/admin/dashboard/__layout.reset.svelte
@@ -58,24 +58,7 @@
 					  href="/admin/dashboard"
 					  class="h-16 px-6 flex flex justify-center items-center w-full
 					  focus:text-orange-500">
-					  <svg
-						  class="h-5 w-5"
-						  xmlns="http://www.w3.org/2000/svg"
-						  width="24"
-						  height="24"
-						  viewBox="0 0 24 24"
-						  fill="none"
-						  stroke="currentColor"
-						  stroke-width="2"
-						  stroke-linecap="round"
-						  stroke-linejoin="round">
-						  <polyline
-							  points="22 12 16 12 14 15 10 15 8 12 2 12"></polyline>
-						  <path
-							  d="M5.45 5.11L2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0
-							  2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0
-							  0-1.79 1.11z"></path>
-					  </svg>
+					  <svg  class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 5v14H5V5h14m1.1-2H3.9c-.5 0-.9.4-.9.9v16.2c0 .4.4.9.9.9h16.2c.4 0 .9-.5.9-.9V3.9c0-.5-.5-.9-.9-.9zM11 7h6v2h-6V7zm0 4h6v2h-6v-2zm0 4h6v2h-6zM7 7h2v2H7zm0 4h2v2H7zm0 4h2v2H7z"/></svg>
 					  <span class="sc-list text-lg pl-2">
 						  Orders
 					  </span>
@@ -87,20 +70,7 @@
 					  href="/admin/dashboard/products"
 					  class="h-16 px-6 flex flex justify-center items-center w-full
 					  focus:text-orange-500">
-					  <svg
-						  class="h-5 w-5"
-						  xmlns="http://www.w3.org/2000/svg"
-						  width="24"
-						  height="24"
-						  viewBox="0 0 24 24"
-						  fill="none"
-						  stroke="currentColor"
-						  stroke-width="2"
-						  stroke-linecap="round"
-						  stroke-linejoin="round">
-						  <path
-							  d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
-					  </svg>
+					  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2l-5.5 9h11z"/><circle cx="17.5" cy="17.5" r="4.5"/><path d="M3 13.5h8v8H3z"/></svg>
 					  <span class="sc-list text-lg pl-2">
 						  Products
 					  </span>
@@ -113,22 +83,7 @@
 					  class="h-16 px-6 flex flex justify-center items-center w-full
 					  focus:text-orange-500">
   
-					  <svg
-						  class="h-5 w-5"
-						  xmlns="http://www.w3.org/2000/svg"
-						  width="24"
-						  height="24"
-						  viewBox="0 0 24 24"
-						  fill="none"
-						  stroke="currentColor"
-						  stroke-width="2"
-						  stroke-linecap="round"
-						  stroke-linejoin="round">
-						  <polyline points="3 6 5 6 21 6"></polyline>
-						  <path
-							  d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2
-							  0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-					  </svg>
+					  <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none"/><path d="M21.41 11.58l-9-9C12.05 2.22 11.55 2 11 2H4c-1.1 0-2 .9-2 2v7c0 .55.22 1.05.59 1.42l9 9c.36.36.86.58 1.41.58.55 0 1.05-.22 1.41-.59l7-7c.37-.36.59-.86.59-1.41 0-.55-.23-1.06-.59-1.42zM5.5 7C4.67 7 4 6.33 4 5.5S4.67 4 5.5 4 7 4.67 7 5.5 6.33 7 5.5 7z"/></svg>
 					  <span class="sc-list text-lg pl-2">
 						  Deals
 					  </span>
@@ -140,23 +95,7 @@
 					  href="/admin/dashboard/reviews"
 					  class="h-16 px-6 flex flex justify-center items-center w-full
 					  focus:text-orange-500">
-					  <svg
-						  class="h-5 w-5"
-						  xmlns="http://www.w3.org/2000/svg"
-						  width="24"
-						  height="24"
-						  viewBox="0 0 24 24"
-						  fill="none"
-						  stroke="currentColor"
-						  stroke-width="2"
-						  stroke-linecap="round"
-						  stroke-linejoin="round">
-						  <circle cx="9" cy="21" r="1"></circle>
-						  <circle cx="20" cy="21" r="1"></circle>
-						  <path
-							  d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0
-							  2-1.61L23 6H6"></path>
-					  </svg>
+					  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><g><path d="M0,0h24v24H0V0z" fill="none"/></g></g><g><path d="M12,8.89L12.94,12h2.82l-2.27,1.62l0.93,3.01L12,14.79l-2.42,1.84l0.93-3.01L8.24,12h2.82L12,8.89 M12,2l-2.42,8H2 l6.17,4.41L5.83,22L12,17.31L18.18,22l-2.35-7.59L22,10h-7.58L12,2L12,2z"/></g></svg>
 					  <span class="sc-list text-lg pl-2">
 						  Reviews
 					  </span>
@@ -165,66 +104,12 @@
   
 			  <li class="hover:bg-gray-100">
 				  <a
-					  href="."
+					  href="/admin/dashboard/settings"
 					  class="h-16 px-6 flex flex justify-center items-center w-full
 					  focus:text-orange-500">
-					  <svg
-						  class="h-5 w-5"
-						  xmlns="http://www.w3.org/2000/svg"
-						  width="24"
-						  height="24"
-						  viewBox="0 0 24 24"
-						  fill="none"
-						  stroke="currentColor"
-						  stroke-width="2"
-						  stroke-linecap="round"
-						  stroke-linejoin="round">
-						  <circle cx="12" cy="12" r="3"></circle>
-						  <path
-							  d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1
-							  0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0
-							  0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2
-							  2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0
-							  0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1
-							  0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0
-							  0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65
-							  0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0
-							  1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0
-							  1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2
-							  0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0
-							  1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0
-							  2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0
-							  0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65
-							  1.65 0 0 0-1.51 1z"></path>
-					  </svg>
+					  <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><path d="M0,0h24v24H0V0z" fill="none"/><path d="M19.14,12.94c0.04-0.3,0.06-0.61,0.06-0.94c0-0.32-0.02-0.64-0.07-0.94l2.03-1.58c0.18-0.14,0.23-0.41,0.12-0.61 l-1.92-3.32c-0.12-0.22-0.37-0.29-0.59-0.22l-2.39,0.96c-0.5-0.38-1.03-0.7-1.62-0.94L14.4,2.81c-0.04-0.24-0.24-0.41-0.48-0.41 h-3.84c-0.24,0-0.43,0.17-0.47,0.41L9.25,5.35C8.66,5.59,8.12,5.92,7.63,6.29L5.24,5.33c-0.22-0.08-0.47,0-0.59,0.22L2.74,8.87 C2.62,9.08,2.66,9.34,2.86,9.48l2.03,1.58C4.84,11.36,4.8,11.69,4.8,12s0.02,0.64,0.07,0.94l-2.03,1.58 c-0.18,0.14-0.23,0.41-0.12,0.61l1.92,3.32c0.12,0.22,0.37,0.29,0.59,0.22l2.39-0.96c0.5,0.38,1.03,0.7,1.62,0.94l0.36,2.54 c0.05,0.24,0.24,0.41,0.48,0.41h3.84c0.24,0,0.44-0.17,0.47-0.41l0.36-2.54c0.59-0.24,1.13-0.56,1.62-0.94l2.39,0.96 c0.22,0.08,0.47,0,0.59-0.22l1.92-3.32c0.12-0.22,0.07-0.47-0.12-0.61L19.14,12.94z M12,15.6c-1.98,0-3.6-1.62-3.6-3.6 s1.62-3.6,3.6-3.6s3.6,1.62,3.6,3.6S13.98,15.6,12,15.6z"/></g></svg>
 					  <span class="sc-list text-lg pl-2">
-						  Something
-					  </span>
-				  </a>
-			  </li>
-  
-			  <li class="hover:bg-gray-100">
-				  <a
-					  href="."
-					  class="h-16 px-6 flex flex justify-center items-center w-full
-					  focus:text-orange-500">
-					  <svg
-						  class="h-5 w-5"
-						  xmlns="http://www.w3.org/2000/svg"
-						  width="24"
-						  height="24"
-						  viewBox="0 0 24 24"
-						  fill="none"
-						  stroke="currentColor"
-						  stroke-width="2"
-						  stroke-linecap="round"
-						  stroke-linejoin="round">
-						  <path
-							  d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"></path>
-						  <path d="M13.73 21a2 2 0 0 1-3.46 0"></path>
-					  </svg>
-					  <span class="sc-list text-lg pl-2">
-						  Something
+						  Settings
 					  </span>
 				  </a>
 			  </li>
---
+++ src/routes/admin/dashboard/settings.svelte
@@ -0,0 +1,11 @@
+<div id="menu" class="mt-12 container mx-auto px-4 lg:pt-24 lg:pb-64">
+    <div class="flex flex-wrap text-center justify-center">
+      <div class="w-full lg:w-6/12 px-4">
+        <h2 class="text-4xl font-semibold text-black">Settings</h2>
+        <p class="text-lg leading-relaxed mt-4 mb-4 text-gray-500">
+            This page contains a few options you can change to make the front-end of your website to look the way prefer. Make sure to Save the settings before logging out
+        </p>
+  
+        </div>
+    </div>
+</div>
