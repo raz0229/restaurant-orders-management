899609d3da0098fc6294f36c61f0f79776d25489
raz0229 <raja.zafar229@gmail.com>
2022-05-20 13:01:55 +0500

    admin: account

--- src/routes/admin/dashboard/account.svelte
+++ src/routes/admin/dashboard/account.svelte
@@ -2,10 +2,12 @@
     import { getContext } from "svelte";
     
     const user = getContext('user-details');
-    let userImpl;
+    let userImpl, email, password, password_confirm;
+    let notPassed = true;
 
     user.subscribe( cred => {
         userImpl = cred;
+        console.log(cred)
     })
 </script>
 
@@ -22,7 +24,91 @@
         <p class="text-lg leading-relaxed mt-4 mb-4 text-gray-500">
             You can simply change your account's email or password through the Account page. Make sure you use a strong password to save it from attackers.
         </p>
-            <p>{ userImpl?.email }</p>
+        </div>
+    </div>
+
+<div class="container max-w-full mx-auto md:py-24 px-6">
+  <div class="max-w-sm mx-auto px-6">
+        <div class="relative flex flex-wrap">
+            <div class="w-full relative">
+                <div class="md:mt-2">
+                    <form on:submit|preventDefault="{()=>alert('Hi')}" 
+                        class="mt-2">
+                        <div class="mx-auto max-w-lg ">
+                            <div class="py-1">
+                                <span class="px-1 text-sm text-gray-600">Email</span>
+                                <input placeholder="{ userImpl?.email }" type="email" bind:value="{email}"
+                                       class="text-md block px-3 py-2 rounded-lg w-full
+                bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none">
+                            </div>
+                            <div class="py-1">
+                                <span class="px-1 text-sm text-gray-600">New Password</span>
+                                <input placeholder="" type="password" bind:value="{password}"
+                                       class="text-md block px-3 py-2 rounded-lg w-full
+                bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none">
+                            </div>
+                            <div class="py-1">
+                                <span class="px-1 text-sm text-gray-600">Confirm Password</span>
+                                <input placeholder="" type="password" bind:value="{password_confirm}"
+                                       class="text-md block px-3 py-2 rounded-lg w-full
+                bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none">
+                            </div>
+                            <div class="flex justify-start mt-3 ml-4 p-1">
+                                <ul>
+                                    <li class="flex items-center py-1 text-red-500">
+                                        <div 
+                                        class=" rounded-full p-1 fill-current ">
+                                            <svg class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path x-show="password == password_confirm && password.length > 0" stroke-linecap="round"
+                                                      stroke-linejoin="round" stroke-width="2"
+                                                      d="M5 13l4 4L19 7"/>
+                                                <path x-show="password != password_confirm || password.length == 0" stroke-linecap="round"
+                                                      stroke-linejoin="round" stroke-width="2"
+                                                      d="M6 18L18 6M6 6l12 12"/>
+
+                                            </svg>
+                                        </div>
+                                        <span 
+                                            class="font-medium text-sm ml-3"
+                                            >Passwords do not match</span>
+                                    </li>
+                                    <li class="flex items-center py-1 text-red-500">
+                                        <div 
+                                            class=" rounded-full p-1 fill-current ">
+                                            <svg class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path x-show="password.length > 7" stroke-linecap="round"
+                                                      stroke-linejoin="round" stroke-width="2"
+                                                      d="M5 13l4 4L19 7"/>
+                                                <path x-show="password.length < 7" stroke-linecap="round"
+                                                      stroke-linejoin="round" stroke-width="2"
+                                                      d="M6 18L18 6M6 6l12 12"/>
+
+                                            </svg>
+                                        </div>
+                                        <span 
+                                            class="font-medium text-sm ml-3"
+                                            >At least 8 characters required</span>
+                                    </li>
+                                </ul>
+                            </div>
+                            <button disabled={notPassed} class="mt-3 text-lg font-semibold
+            bg-indigo-700 disabled:bg-gray-400 w-full text-white rounded-lg
+            px-6 py-3 block shadow-xl hover:text-white hover:bg-indigo-900">
+                                Update
+                            </button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
         </div>
     </div>
 </div>
+
+</div>
+
+<style>
+    input {
+        outline: none;
+    }
+</style>
