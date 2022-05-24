aa249d348acba670c308ff4fd97f9a62ed48e617
raz0229 <raja.zafar229@gmail.com>
2022-05-24 19:42:15 +0500

    BDD: homepage test

--- cypress/integration/ui/home.js
+++ cypress/integration/ui/home.js
@@ -1,15 +1,49 @@
 describe('UI - Home page', () => {
     it('Visits the home page', () => {
       cy.visit(Cypress.env('base_url'))
-    
-      //cy.contains('type').click()
+    })
 
-      cy.title().should('include', 'Home')
+    it('Checks title and scroll bottom smoothly', () => {
 
-    //   cy.get('.action-email')
-    //   .type('fake@email.com')
-    //   .should('have.value', 'fake@email.com')
-     
+        cy.title().should('include', 'Home')
+  
+        cy.scrollTo('bottom', { easing: 'linear', duration: 5000 })
+  
+      })
 
+    it('Canvas renders the images in deals', () => {
+
+        cy.get('.zoom').first().scrollIntoView()
+
+        cy.get('.zoom')
+        .children()
+        .should(($div) => {
+  
+          const src = $div[0].getAttribute('src')
+          console.log('src: ', $div[0].getAttribute('src'))
+          expect(src).to.have.length.greaterThan(0)
+        })   
+  
+      })
+  
+    it('Should switch to dark mode', () => {
+
+        cy.scrollTo('top')
+        cy.contains('dark_mode').click()
+        cy.scrollTo('bottom', { easing: 'linear', duration: 3000 })
+    })
+
+    it('Opens cart from right-side', () => {
+        cy.contains('shopping_cart').click()
+    })
+
+    it('Clear cart if not and close', () => {
+        cy.contains('Clear Cart').click()
+
+        // check if cart clear was successfull
+        cy.get('tbody')
+        .children()
+        .should('have.length', 0)
+        cy.contains('shopping_cart').click()
     })
   })
