d8e3c265491196903c8d68dc81c7bfd6600e50e5
raz0229 <raja.zafar229@gmail.com>
2022-05-27 20:15:56 +0500

    test ui: reviews page

--- cypress/integration/ui/reviews.js
+++ cypress/integration/ui/reviews.js
@@ -3,51 +3,54 @@ describe('UI - Reviews page', () => {
       cy.visit(Cypress.env('base_url') + '/reviews')
     })
 
-    // it('Checks title and scroll bottom smoothly', () => {
+    it('Checks title and scroll bottom smoothly', () => {
 
-    //     cy.title().should('include', 'Reviews')
+        cy.title().should('include', 'Reviews')
   
-    //     cy.scrollTo('bottom', { easing: 'linear', duration: 3000 })
+        cy.scrollTo('bottom', { easing: 'linear', duration: 3000 })
   
-    //   })
+      })
 
-    // it('Should switch to dark mode', () => {
+    it('Should switch to dark mode', () => {
 
-    //     cy.scrollTo('top')
-    //     cy.contains('dark_mode').click()
-    //     cy.scrollTo('bottom', { easing: 'linear', duration: 3000 })
-    // })
+        cy.scrollTo('top')
+        cy.contains('dark_mode').click()
+        cy.scrollTo('bottom', { easing: 'linear', duration: 3000 })
+    })
 
-    // it('Load more reviews and try sort', () => {
+    it('Load more reviews and try sort', () => {
 
-    //     /* Run get_data.js from API to test reviews */
-    //     cy.contains('Load more').click()
-    //     cy.scrollTo('top')
-    //     cy.get('select').select('oldest')
-    //     cy.get('select').select('latest')
-    //     cy.get('select').select('title')
-    // })
+        /* Run get_data.js from API to test reviews thououghly*/
+        cy.contains('Load more').click()
+        cy.scrollTo('top')
+        cy.get('select').select('oldest')
+        cy.get('select').select('latest')
+        cy.get('select').select('title')
+    })
+
+
+    it('Opens cart from right-side', () => {
+        cy.contains('shopping_cart').click()
+    })
+
+    it('Clear cart if not and close', () => {
+        cy.contains('Clear Cart').click()
+
+        // check if cart clear was successfull
+        cy.get('tbody')
+        .children()
+        .should('have.length', 0)
+        cy.contains('shopping_cart').click()
+    })
 
     it('Write a review', () => {
+        /* Run get_data.js from API to test reviews submission*/
+
         cy.contains('Write a Review').click()
         cy.contains('Your Name').next().type('John Doe')
         cy.contains('Your Email').next().type('johndoe@gmail.com')
         cy.contains('Comment').next().type('This review is a test sumbission. Please delete it')
         cy.contains('Submit').should('not.be.disabled').click()
-        cy.get('#write-review-modal').should('not.be.visible')
     })
 
-    // it('Opens cart from right-side', () => {
-    //     cy.contains('shopping_cart').click()
-    // })
-
-    // it('Clear cart if not and close', () => {
-    //     cy.contains('Clear Cart').click()
-
-    //     // check if cart clear was successfull
-    //     cy.get('tbody')
-    //     .children()
-    //     .should('have.length', 0)
-    //     cy.contains('shopping_cart').click()
-    // })
   })
