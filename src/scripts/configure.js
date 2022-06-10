e5679516aefd8328047f519e1c6a6152eeceecc0
raz0229 <raja.zafar229@gmail.com>
2022-06-10 18:51:33 +0500

    configure script

--- src/scripts/configure.js
+++ src/scripts/configure.js
@@ -9,6 +9,7 @@ Data added can be updated or removed entirely through the admin panel
 */
 
 import { db, auth } from "../lib/config/app.js"
+import { doc, setDoc } from "firebase/firestore"; 
 import { signInWithEmailAndPassword } from "firebase/auth"
 import promptSync  from "prompt-sync"
 import { createSpinner } from 'nanospinner'
@@ -22,8 +23,22 @@ console.clear()
 try {
     await signInWithEmailAndPassword(auth, email.trim(), pswd)
     spinner.success()
-    console.log("\x1b[32m", 'Signed in successfully!')
+    console.log("\x1b[32m", '[CONFIG] Signed in successfully!')
+    await addSettings()
 } catch (err) {
     spinner.error()
-    console.log("\x1b[31m", 'Wrong email or password!')
+    console.log("\x1b[31m", '[CONFIG] Something went wrong!')
+    process.exit()
+}
+
+const addSettings = async () => {
+    try {
+        await setDoc(doc(db, "settings2", "LA"), {
+            name: "Los Angeles",
+            state: "CA",
+            country: "USA"
+          });
+    } catch (err) {
+        console.log(err)
+    }
 }
