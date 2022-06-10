0d8060d373e643dc98db83ffcf175cc184ceaf76
raz0229 <raja.zafar229@gmail.com>
2022-06-10 21:56:54 +0500

    [+ dummy.js]

--- src/scripts/configure.js
+++ src/scripts/configure.js
@@ -13,12 +13,12 @@ import { doc, setDoc } from "firebase/firestore";
 import { signInWithEmailAndPassword } from "firebase/auth"
 import promptSync  from "prompt-sync"
 import { createSpinner } from 'nanospinner'
+import { reviews, settings } from "./dummy.js"
 
 const prompt = promptSync()
 const email = prompt("Enter Email: ")
 const pswd = prompt.hide("Enter Password: ")
 const spinner = createSpinner(' Signing in').start()
-
 
 const setDocuments = (label, map, colName) => {
     return new Promise(async (resolve, reject) => {
@@ -39,106 +39,6 @@ const setDocuments = (label, map, colName) => {
         }
     })
 }
-
-const settings = new Map([
-    ["active-hours", {
-        startUTC: 3,
-        endUTC: 18
-    }],
-    ["delivery-charges", {
-        val: 200
-    }],
-    ["max-deals", {
-        val: 6
-    }],
-    ["max-reviews", {
-        val: 6
-    }]
-])
-
-const reviews = new Map([
-    ["0RnB3NnbiqGYc6jEz24a",{
-        title: 'Adam Janjua',
-        stars: 4,
-        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        date: 'Mar 23 2022',
-        email: 'alijinjua@gmail.com',
-        latest: -164804712,
-        oldest: 164804712
-    }],
-    ["0RnB3NnbiqGYc6jEz24b",{
-        title: 'George Bush Jr',
-        stars: 3,
-        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        date: 'Mar 23 2022',
-        email: 'usstatedept@hotmail.com',
-        latest: -164804715,
-        oldest: 164804715
-    }],
-    ["0RnB3NnbiqGYc6jEz24c",{
-        title: 'Crystal Anne',
-        stars: 5,
-        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        date: 'May 07 2022',
-        email: 'jellymello848@gmail.com',
-        latest: -165192387,
-        oldest: 165192387
-    }],
-    ["0RnB3NnbiqGYc6jEz24d",{
-        title: 'Qween Hun',
-        stars: 5,
-        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        date: 'Mar 24 2022',
-        email: 'supremeleader@hunsvil.le',
-        latest: -164813943,
-        oldest: 164813943
-    }],
-    ["0RnB3NnbiqGYc6jEz24e",{
-        title: 'Mustafa Goku',
-        stars: 4,
-        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        date: 'Mar 24 2022',
-        email: 'goku12334@gmail.com',
-        latest: -164813936,
-        oldest: 164813936
-    }],
-    ["0RnB3NnbiqGYc6jEz24f",{
-        title: 'Majeed Don',
-        stars: 3,
-        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        date: 'Mar 23 2022',
-        email: 'fluffpgc@outlook.com',
-        latest: -164804705,
-        oldest: 164804705
-    }],
-    ["0RnB3NnbiqGYc6jEz24g",{
-        title: 'Miss Jane',
-        stars: 5,
-        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        date: 'Mar 23 2022',
-        email: 'missjanelle@gmail.com',
-        latest: -164804702,
-        oldest: 164804702
-    }],
-    ["0RnB3NnbiqGYc6jEz24h",{
-        title: 'Raja Abdullah Zafar',
-        stars: 4,
-        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        date: 'Mar 23 2022',
-        email: 'raja.zafar229@gmail.com',
-        latest: -164804700,
-        oldest: 164804700
-    }],
-    ["0RnB3NnbiqGYc6jEz24i",{
-        title: 'Motayyab Jabbar',
-        stars: 4,
-        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        date: 'Mar 24 2022',
-        email: 'motayyabjabbar123@yahoo.com',
-        latest: -164814119,
-        oldest: 164814119
-    }]
-])
 
 console.clear()
 try {
