fa9d19b22b94af7977a4afd5bf5703e644971913
raz0229 <raja.zafar229@gmail.com>
2022-06-10 21:47:03 +0500

    dummy data: 'reviews' added in config script

--- src/scripts/configure.js
+++ src/scripts/configure.js
@@ -57,86 +57,86 @@ const settings = new Map([
 ])
 
 const reviews = new Map([
-    ["",{
-        title: '',
+    ["0RnB3NnbiqGYc6jEz24a",{
+        title: 'Adam Janjua',
         stars: 4,
         comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        date: '',
-        email: '',
-        latest: '',
-        oldest: ''
+        date: 'Mar 23 2022',
+        email: 'alijinjua@gmail.com',
+        latest: -164804712,
+        oldest: 164804712
     }],
-    ["",{
-        title: '',
-        stars: 4,
+    ["0RnB3NnbiqGYc6jEz24b",{
+        title: 'George Bush Jr',
+        stars: 3,
         comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        date: '',
-        email: '',
-        latest: '',
-        oldest: ''
+        date: 'Mar 23 2022',
+        email: 'usstatedept@hotmail.com',
+        latest: -164804715,
+        oldest: 164804715
     }],
-    ["",{
-        title: '',
-        stars: 4,
+    ["0RnB3NnbiqGYc6jEz24c",{
+        title: 'Crystal Anne',
+        stars: 5,
         comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        date: '',
-        email: '',
-        latest: '',
-        oldest: ''
+        date: 'May 07 2022',
+        email: 'jellymello848@gmail.com',
+        latest: -165192387,
+        oldest: 165192387
     }],
-    ["",{
-        title: '',
-        stars: 4,
+    ["0RnB3NnbiqGYc6jEz24d",{
+        title: 'Qween Hun',
+        stars: 5,
         comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        date: '',
-        email: '',
-        latest: '',
-        oldest: ''
+        date: 'Mar 24 2022',
+        email: 'supremeleader@hunsvil.le',
+        latest: -164813943,
+        oldest: 164813943
     }],
-    ["",{
-        title: '',
+    ["0RnB3NnbiqGYc6jEz24e",{
+        title: 'Mustafa Goku',
         stars: 4,
         comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        date: '',
-        email: '',
-        latest: '',
-        oldest: ''
+        date: 'Mar 24 2022',
+        email: 'goku12334@gmail.com',
+        latest: -164813936,
+        oldest: 164813936
     }],
-    ["",{
-        title: '',
-        stars: 4,
+    ["0RnB3NnbiqGYc6jEz24f",{
+        title: 'Majeed Don',
+        stars: 3,
         comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        date: '',
-        email: '',
-        latest: '',
-        oldest: ''
+        date: 'Mar 23 2022',
+        email: 'fluffpgc@outlook.com',
+        latest: -164804705,
+        oldest: 164804705
     }],
-    ["",{
-        title: '',
-        stars: 4,
+    ["0RnB3NnbiqGYc6jEz24g",{
+        title: 'Miss Jane',
+        stars: 5,
         comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        date: '',
-        email: '',
-        latest: '',
-        oldest: ''
+        date: 'Mar 23 2022',
+        email: 'missjanelle@gmail.com',
+        latest: -164804702,
+        oldest: 164804702
     }],
-    ["",{
-        title: '',
+    ["0RnB3NnbiqGYc6jEz24h",{
+        title: 'Raja Abdullah Zafar',
         stars: 4,
         comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        date: '',
-        email: '',
-        latest: '',
-        oldest: ''
+        date: 'Mar 23 2022',
+        email: 'raja.zafar229@gmail.com',
+        latest: -164804700,
+        oldest: 164804700
     }],
-    ["",{
-        title: '',
+    ["0RnB3NnbiqGYc6jEz24i",{
+        title: 'Motayyab Jabbar',
         stars: 4,
         comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-        date: '',
-        email: '',
-        latest: '',
-        oldest: ''
+        date: 'Mar 24 2022',
+        email: 'motayyabjabbar123@yahoo.com',
+        latest: -164814119,
+        oldest: 164814119
     }]
 ])
 
@@ -146,7 +146,8 @@ try {
     spinner.success()
     console.log("\x1b[32m", '[CONFIG] Signed in successfully!')
 
-    await setDocuments(' [Creating Collection] Settings', settings, "settings2")
+    await setDocuments(' [Creating Collection] Settings', settings, "settings")
+    await setDocuments(' [Creating Collection] Reviews', reviews, "reviews")
 
     process.exit()
 
