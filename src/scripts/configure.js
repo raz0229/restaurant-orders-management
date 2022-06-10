e66e1986d8c4215ed369bdfdae8e643f5eefdb41
raz0229 <raja.zafar229@gmail.com>
2022-06-10 21:06:58 +0500

    bug fix: asynchorous code in configure script

--- src/scripts/configure.js
+++ src/scripts/configure.js
@@ -19,26 +19,47 @@ const email = prompt("Enter Email: ")
 const pswd = prompt.hide("Enter Password: ")
 const spinner = createSpinner(' Signing in').start()
 
+const settings = new Map([
+    ["active-hours", {
+        startUTC: 3,
+        endUTC: 18
+    }],
+    ["delivery-charges", {
+        val: 200
+    }],
+    ["max-deals", {
+        val: 6
+    }],
+    ["max-reviews", {
+        val: 6
+    }]
+])
+
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
 console.clear()
 try {
     await signInWithEmailAndPassword(auth, email.trim(), pswd)
     spinner.success()
     console.log("\x1b[32m", '[CONFIG] Signed in successfully!')
-    await addSettings()
+
+    spinner.update({
+        text: ' [Creating Collection] Settings'
+    }).start()
+    // for await (const contents of settings) {
+    //     console.log(contents)
+    // }
+    for (const [key, value] of settings) {
+        console.log(key, value)
+        //await setDoc(doc(db, "settings2", key), value);
+        await delay(1000)
+    }
+
+    console.log("finished")
+    spinner.success()
+
 } catch (err) {
     spinner.error()
     console.log("\x1b[31m", '[CONFIG] Something went wrong!')
     process.exit()
-}
-
-const addSettings = async () => {
-    try {
-        await setDoc(doc(db, "settings2", "LA"), {
-            name: "Los Angeles",
-            state: "CA",
-            country: "USA"
-          });
-    } catch (err) {
-        console.log(err)
-    }
 }
