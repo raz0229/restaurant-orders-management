af30aadbdd86a81cfc69b13886d09e69c90ae85c
raz0229 <raja.zafar229@gmail.com>
2022-06-10 21:30:41 +0500

    script configure: review3s blueprint

--- src/scripts/configure.js
+++ src/scripts/configure.js
@@ -19,6 +19,27 @@ const email = prompt("Enter Email: ")
 const pswd = prompt.hide("Enter Password: ")
 const spinner = createSpinner(' Signing in').start()
 
+
+const setDocuments = (label, map, colName) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            spinner.update({
+                text: label
+            }).start()
+            
+            for (const [key, value] of map) {
+                await setDoc(doc(db, colName, key), value);
+            }
+            
+            spinner.success()
+            resolve(map)
+
+        } catch (err) {
+            reject(err)
+        }
+    })
+}
+
 const settings = new Map([
     ["active-hours", {
         startUTC: 3,
@@ -35,21 +56,98 @@ const settings = new Map([
     }]
 ])
 
+const reviews = new Map([
+    ["",{
+        title: '',
+        stars: 4,
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        date: '',
+        email: '',
+        latest: '',
+        oldest: ''
+    }],
+    ["",{
+        title: '',
+        stars: 4,
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        date: '',
+        email: '',
+        latest: '',
+        oldest: ''
+    }],
+    ["",{
+        title: '',
+        stars: 4,
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        date: '',
+        email: '',
+        latest: '',
+        oldest: ''
+    }],
+    ["",{
+        title: '',
+        stars: 4,
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        date: '',
+        email: '',
+        latest: '',
+        oldest: ''
+    }],
+    ["",{
+        title: '',
+        stars: 4,
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        date: '',
+        email: '',
+        latest: '',
+        oldest: ''
+    }],
+    ["",{
+        title: '',
+        stars: 4,
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        date: '',
+        email: '',
+        latest: '',
+        oldest: ''
+    }],
+    ["",{
+        title: '',
+        stars: 4,
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        date: '',
+        email: '',
+        latest: '',
+        oldest: ''
+    }],
+    ["",{
+        title: '',
+        stars: 4,
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        date: '',
+        email: '',
+        latest: '',
+        oldest: ''
+    }],
+    ["",{
+        title: '',
+        stars: 4,
+        comment: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        date: '',
+        email: '',
+        latest: '',
+        oldest: ''
+    }]
+])
+
 console.clear()
 try {
     await signInWithEmailAndPassword(auth, email.trim(), pswd)
     spinner.success()
     console.log("\x1b[32m", '[CONFIG] Signed in successfully!')
 
-    spinner.update({
-        text: ' [Creating Collection] Settings'
-    }).start()
-    
-    for (const [key, value] of settings) {
-        await setDoc(doc(db, "settings2", key), value);
-    }
+    await setDocuments(' [Creating Collection] Settings', settings, "settings2")
 
-    spinner.success()
     process.exit()
 
 } catch (err) {
