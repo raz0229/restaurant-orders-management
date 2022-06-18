30f6e86cc7298d1044d8e5c5bfcbb52c0da2ec2c
raz0229 <raja.zafar229@gmail.com>
2022-06-18 21:49:28 +0500

    npm run configure: finished

--- src/scripts/configure.js
+++ src/scripts/configure.js
@@ -9,117 +9,141 @@ Data added can be updated or removed entirely through the admin panel
 */
 
 import { db, auth, client } from "../lib/config/app.js"
-import { doc, setDoc } from "firebase/firestore"; 
+import { doc, setDoc } from "firebase/firestore";
 import { signInWithEmailAndPassword } from "firebase/auth"
-import promptSync  from "prompt-sync"
+import promptSync from "prompt-sync"
 import { createSpinner } from 'nanospinner'
-import { reviews, deals, settings, products, groups} from "./dummy.js"
-import {basename} from 'path'
-import {createReadStream} from 'fs'
+import { reviews, deals, settings, products, groups } from "./dummy.js"
+import { basename } from 'path'
+import { createReadStream } from 'fs'
 
-groups.forEach( group => {
-  let ddoc, products = [];
+const prompt = promptSync()
+const email = prompt("Enter Email: ")
+const pswd = prompt.hide("Enter Password: ")
+const spinner = createSpinner(' Signing in').start()
 
-  ddoc = {
-      _id: group.id,
-      _type: 'group',
-      title: group.title,
-      priority: group.priority,
-      sizes: group.sizes
+const setDocuments = (label, map, colName) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      spinner.update({
+        text: label
+      }).start()
+
+      for (const [key, value] of map) {
+        await setDoc(doc(db, colName, key), value);
+      }
+
+      spinner.success()
+      resolve(map)
+
+    } catch (err) {
+      reject(err)
     }
+  })
+}
 
-  group.products.forEach(ref => {
-    products.push({
-      _key: `key-${ref}`,
-      _ref: ref,
-      _type: 'reference'
+const setGroups = () => {
+  return new Promise(async resolve => {
+    // create groups array filled with products
+    groups.forEach(async (group, count) => {
+      let ddoc, products = [];
+
+      ddoc = {
+        _id: group.id,
+        _type: 'group',
+        title: group.title,
+        priority: group.priority,
+        sizes: group.sizes
+      }
+
+      group.products.forEach(ref => {
+        products.push({
+          _key: `key-${ref}`,
+          _ref: ref,
+          _type: 'reference'
+        })
+      })
+
+      ddoc.products = products;
+
+      await client.createOrReplace(ddoc)
+
+      spinner.update({
+        text: `[Creating Groups] ${100 / groups.length * (count + 1)}%`
+      }).start()
+      if (count === (groups.length - 1)) {
+        spinner.success()
+        resolve(groups)
+      }
+
     })
   })
+}
 
-  ddoc.products = products;
+const setProducts = () => {
+  return new Promise(async resolve => {
 
-  client.createOrReplace(ddoc).then(() => {
+    // create products with dummy data
+    products.forEach(async (product, count) => {
+
+      let ddoc;
+      const filePath = `static/menu/${product.fileName}`
+
+      const imageAsset = await client.assets
+        .upload('image', createReadStream(filePath), {
+          filename: basename(filePath)
+        })
+
+        // Here you can decide what to do with the returned asset document. 
+        // If you want to set a specific asset field you can to the following:
+        ddoc = {
+          _id: product.id,
+          _type: 'product',
+          title: product.title,
+          slug: {
+            _type: 'slug',
+            current: product.slug
+          },
+          prices: product.prices,
+          image: {
+            _type: 'image',
+            asset: {
+              _ref: imageAsset._id,
+              _type: 'reference'
+              }
+            }
+          }
+        
+        await client.createOrReplace(ddoc)
+        spinner.update({
+          text: `[Creating Products] ${100 / products.length * (count + 1)}%`
+        }).start()
+        if (count === (products.length - 1)) {
+          spinner.success()
+          resolve(products)
+        }
+
+    });
 
   })
-})
+}
 
-// products.forEach( product => {
+console.clear()
+try {
+  await signInWithEmailAndPassword(auth, email.trim(), pswd)
+  spinner.success()
+  console.log("\x1b[32m", '[CONFIG] Signed in successfully!')
 
-//   let ddoc;
-//   const filePath = `static/menu/${product.fileName}`
-  
-//   client.assets
-//     .upload('image', createReadStream(filePath), {
-//       filename: basename(filePath)
-//     })
-//     .then(imageAsset => {
-//       // Here you can decide what to do with the returned asset document. 
-//       // If you want to set a specific asset field you can to the following:
-//       ddoc = {
-//           _id: product.id,
-//           _type: 'product',
-//           title: product.title,
-//           slug: {
-//               _type: 'slug',
-//               current: product.slug
-//           },
-//           prices: product.prices,
-//           image: {
-//               _type: 'image',
-//               asset: {
-//                   _ref: imageAsset._id,
-//                   _type: 'reference'
-//               }
-//           }
-//       }
-//   })
-//     .then(() => {
-//       console.log(ddoc);
-//       client.createOrReplace(ddoc)
-//     })
-  
+  await setDocuments(' [Creating Collection] Settings', settings, "settings")
+  await setDocuments(' [Creating Collection] Reviews', reviews, "reviews")
+  await setDocuments(' [Creating Collection] Deals', deals, "deals")
+  await setProducts()
+  await setGroups()
 
-// });
+  process.exit()
 
-// const prompt = promptSync()
-// const email = prompt("Enter Email: ")
-// const pswd = prompt.hide("Enter Password: ")
-// const spinner = createSpinner(' Signing in').start()
-
-// const setDocuments = (label, map, colName) => {
-//     return new Promise(async (resolve, reject) => {
-//         try {
-//             spinner.update({
-//                 text: label
-//             }).start()
-            
-//             for (const [key, value] of map) {
-//                 await setDoc(doc(db, colName, key), value);
-//             }
-            
-//             spinner.success()
-//             resolve(map)
-
-//         } catch (err) {
-//             reject(err)
-//         }
-//     })
-// }
-
-// //console.clear()
-// try {
-//     await signInWithEmailAndPassword(auth, email.trim(), pswd)
-//     spinner.success()
-//     console.log("\x1b[32m", '[CONFIG] Signed in successfully!')
-
-//     await setDocuments(' [Creating Collection] Settings', settings, "settings")
-//     await setDocuments(' [Creating Collection] Reviews', reviews, "reviews")
-//     await setDocuments(' [Creating Collection] Deals', deals, "deals")
-
-//     process.exit()
-
-// } catch (err) {
-//     spinner.error()
-//     console.log("\x1b[31m", '[CONFIG] Something went wrong!')
-//     process.exit()
-// }
+} catch (err) {
+  spinner.error()
+  console.log("\x1b[31m", '[CONFIG] Something went wrong!')
+  process.exit()
+}
